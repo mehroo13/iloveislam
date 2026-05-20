1be7e5d4978a18008434cf54481607c4519006ae
mehroo13 <mehrankhan212257@gmail.com>
2026-05-20 20:53:38 +1000

    Added new tool

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -35,7 +35,8 @@ export default function BlogPage() {
                 </span>
               </h2>
 
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {/* Updated Grid - 2 columns on mobile */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {categoryArticles.map(article => (
                   <Link 
                     key={article.slug} 
